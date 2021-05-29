0d8cbd6bf0b597d41811cc65538173713763ef84
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-29 11:57:42 -0400

    creating a quiz route files. adding a simple route to quizes

---
+++ routes/quizes.js
@@ -0,0 +1,21 @@
+// All routes related to quizes
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    res.send("Quizes Routes")
+    // db.query(`SELECT * FROM users;`)
+    //   .then(data => {
+    //     const users = data.rows;
+    //     res.json({ users });
+    //   })
+    //   .catch(err => {
+    //     res
+    //       .status(500)
+    //       .json({ error: err.message });
+    //   });
+  });
+  return router;
+};

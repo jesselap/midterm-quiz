780c68ec0d62873083fbcb1e3dbd774b78701c66
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-29 12:16:24 -0400

    connecting create_new quiz page form to the poast method on the quiz routes

--- routes/quizes.js
+++ routes/quizes.js
@@ -5,13 +5,24 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-
     const queryContent = `SELECT * FROM quizes;`
     db.query(queryContent).then(data => res.json(data.rows))
       .catch(err => res.json(err))
   });
-  router.get("/create", (req, res) => {
+
+  router.get("/new", (req, res) => {
     res.render('create_quiz')
+  });
+
+  router.post("/", (req, res) => {
+    res.send("POST METHOD")
   });
   return router;
 };
+
+// possible route
+
+// /quizes GET (all quizes in a list or box 4/3 in a row)
+// /quizes/new GET (Show form to make new Quiz)
+// Quizes POST (Insert new quiz to the database)
+// Quizes/:id GET (show specific quiz)

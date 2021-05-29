f7036761d7150a77cde7532e600e2aa04c412280
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-29 12:21:24 -0400

    implementing routing for the specific quizes

--- routes/quizes.js
+++ routes/quizes.js
@@ -1,12 +1,29 @@
 // All routes related to quizes
+// possible routes
+
+// /quizes GET (all quizes in a list or box 4/3 in a row)
+// /quizes/new GET (Show form to make new Quiz)
+// Quizes POST (Insert new quiz to the database)
+// Quizes/:id GET (show specific quiz)
+
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const queryContent = `SELECT * FROM quizes;`
     db.query(queryContent).then(data => res.json(data.rows))
+      .catch(err => res.json(err))
+  });
+
+  router.get("/:id", (req, res) => {
+    const queryContent = `
+                          SELECT * FROM quizes
+                          WHERE id = ${req.params.id};
+                         `;
+    db.query(queryContent)
+      .then(data => res.json(data.rows))
       .catch(err => res.json(err))
   });
 
@@ -19,10 +36,3 @@ module.exports = (db) => {
   });
   return router;
 };
-
-// possible route
-
-// /quizes GET (all quizes in a list or box 4/3 in a row)
-// /quizes/new GET (Show form to make new Quiz)
-// Quizes POST (Insert new quiz to the database)
-// Quizes/:id GET (show specific quiz)

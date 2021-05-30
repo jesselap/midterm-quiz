3ebb55f9213cee30d014e113b4134d586643cd63
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-30 17:26:24 -0400

    removed hardcoded html, HTML now generating using jquery

--- public/scripts/quizes.js
+++ public/scripts/quizes.js
@@ -1,25 +1,34 @@
-const createElement = (quiz) => {
-  const quiz =
-  `<div class="card bg-dark text-white col-md-4">
+const createElement = function (quizObj) {
+  const quizHTML =
+    `<div class="card bg-dark text-white col-md-4">
     <img
       src="https://i.guim.co.uk/img/media/5cbce71c025dd78ca31d03111bd2ee4453a7029e/0_167_2400_1440/master/2400.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=b44c9a27a5b38c0388b092e5b0291c32"
       class="card-img" alt="...">
     <div class="overlay">
-      <span>${quiz.category}</span>
-      <p class="card-text">${quiz.title}</p>
+      <span>${quizObj.category}</span>
+      <p class="card-text">${quizObj.title}</p>
     </div>
   </div>`
+  return quizHTML
 }
 
-$( document ).ready(function() {
+const renderQuiz = function (quizes) {
+  for (const item of quizes) {
+    const quizElement = createElement(item)
+    console.log(quizElement)
+    $('.quizes-container').append(quizElement)
+  }
+}
+
+$(document).ready(function () {
   console.log('Jquery Working');
-  console.log($);
-  $('.card').on('click', function() {
+  $('.card').on('click', function () {
     console.log('clicked')
   })
   $.get('/quizes', function (data) {
-    console.log("Inside get")
-    console.log(data)
+    // console.log("Inside get")
+    // console.log(data)
+    renderQuiz(data)
   });
 });
 

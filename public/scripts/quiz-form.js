ea632bae0414806aab390848fa10911593bc5533
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-31 23:31:07 -0400

    adding hrefs to nav elements which are working for now

--- public/scripts/quiz-form.js
+++ public/scripts/quiz-form.js
@@ -3,23 +3,23 @@ const createQuizHTML = function(no) {
   `
     <div class="col-12">
     <label for="question1">Question No: ${no}</label>
-    <input type="text" class="form-control" id="question1" name="questions[${no}]" placeholder="First Question">
+    <input type="text" class="form-control" id="question1" name="questions[${no}]" placeholder="First Question" required>
     </div>
     <div class="col-md-6">
     <label for="answer">Your Answer</label>
-    <input type="text" class="form-control" id="answer" name="answers[${no}]" placeholder="Your Answer">
+    <input type="text" class="form-control" id="answer" name="answers[${no}]" placeholder="Your Answer" required>
     </div>
     <div class="col-md-6">
     <label for="optionA">Option A</label>
-    <input type="text" class="form-control" id="optionA" name="optionA[${no}]" placeholder="Option A">
+    <input type="text" class="form-control" id="optionA" name="optionA[${no}]" placeholder="Option A" required>
     </div>
     <div class="col-md-6">
     <label for="optionA">Option B</label>
-    <input type="text" class="form-control" id="optionB" name="optionB[${no}]" placeholder="Option B">
+    <input type="text" class="form-control" id="optionB" name="optionB[${no}]" placeholder="Option B" required>
     </div>
     <div class="col-md-6">
     <label for="optionA">Option C</label>
-    <input type="text" class="form-control" id="optionC" name="optionC[${no}]" placeholder="Option C">
+    <input type="text" class="form-control" id="optionC" name="optionC[${no}]" placeholder="Option C" required>
     <br>
     <br>
     </div>
@@ -36,7 +36,7 @@ const renderQuestion = function(count) {
 }
 
 $(document).ready(function () {
-  renderQuestion(2);
+  renderQuestion(5);
   renderQuestion(this.value);
   $('#questionCount').change(function () {
     $('#questions-container div').remove();

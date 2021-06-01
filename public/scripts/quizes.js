a885e872cb74f93ce67a9c8aacd95090d7df9715
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-06-01 12:47:25 -0400

    adding imageurl and quiz title for the quizes

--- public/scripts/quizes.js
+++ public/scripts/quizes.js
@@ -2,7 +2,7 @@ const createElement = function (quizObj) {
   const quizHTML =
     `<div class="card bg-dark text-white col-md-4">
     <img
-      src="https://i.guim.co.uk/img/media/5cbce71c025dd78ca31d03111bd2ee4453a7029e/0_167_2400_1440/master/2400.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=b44c9a27a5b38c0388b092e5b0291c32"
+      src="https://images.unsplash.com/photo-1532094349884-543bc11b234d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
       class="card-img" alt="...">
     <div class="overlay">
       <span>${quizObj.category}</span>

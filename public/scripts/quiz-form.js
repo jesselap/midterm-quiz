b8ec78741409f78e038665f9132a2e9f9e3ec31d
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-31 21:55:31 -0400

    creating a function to dynamically generate questions

--- public/scripts/quiz-form.js
+++ public/scripts/quiz-form.js
@@ -1,7 +1,36 @@
-$(document).ready(function() {
-console.log('jquery is ready')
-// $('#quiz-form').submit(function(e) {
-//   e.preventDefault();
-//   console.log("Form has been submitted")
-// })
+const createQuizHTML = function(no) {
+  const quizHTML =
+  `
+    <div class="col-12">
+    <label for="question1">Question No: ${no}/label>
+    <input type="text" class="form-control" id="question1" name="questions[${no}]" placeholder="First Question">
+    </div>
+    <div class="col-md-6">
+    <label for="answer">Your Answer</label>
+    <input type="text" class="form-control" id="answer" name="answers[${no}]" placeholder="Your Answer">
+    </div>
+    <div class="col-md-6">
+    <label for="optionA">Option A</label>
+    <input type="text" class="form-control" id="optionA" name="optionA[${no}]" placeholder="Option A">
+    </div>
+    <div class="col-md-6">
+    <label for="optionA">Option B</label>
+    <input type="text" class="form-control" id="optionB" name="optionB[${no}]" placeholder="Option B">
+    </div>
+    <div class="col-md-6">
+    <label for="optionA">Option C</label>
+    <input type="text" class="form-control" id="optionC" name="optionC[${no}]" placeholder="Option C">
+    </div>
+  `
+  return quizHTML;
+}
+
+$(document).ready(function () {
+  $('#questionCount').change(function () {
+    const count = $('#questionCount').val();
+    console.log(count)
+    for(const i = 0; i < count; i++) {
+
+    }
+  })
 })

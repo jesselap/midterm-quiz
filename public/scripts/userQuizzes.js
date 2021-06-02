cedd992ce3f74c65d57ee8abf8f21e9af820a367
Jaemin Han <j4emin.han@gmail.com>
2021-06-02 22:01:28 +0000

    implement overlay buttons to make quizzes shareable on user page

---
+++ public/scripts/userQuizzes.js
@@ -0,0 +1,19 @@
+$(() => {
+  $(".shareLink").hide();
+  $(".share").on("click", function() {
+      /* Get the text field */
+      var copyText = document.getElementById(`linkField${$(this).attr("id")}`);
+
+      /* Select the text field */
+      copyText.select();
+      copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+      /* Copy the text inside the text field */
+      document.execCommand("copy");
+
+      /* Alert the copied text */
+      $(this).text("Copied!");
+  })
+  $(".card-img-overlay").animate({opacity: 0}, 1);
+  $(".card").hover(function() { $(this).find(".card-img-overlay").animate({opacity: 1}, 250)}, function() {$(this).find(".card-img-overlay").animate({opacity: 0}, 250)});
+})
